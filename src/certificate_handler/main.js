34e4e9445a04c2ec7966acb27a72cacdc59a678d
kaustavy2k <kaustavbasaky2k@gmail.com>
2020-10-27 20:27:53 +0530

    new pdf

--- src/certificate_handler/main.js
+++ src/certificate_handler/main.js
@@ -46,8 +46,8 @@ async function modifyPdf(name) {
   const Font = await pdfDoc.embedFont(StandardFonts.TimesRomanBoldItalic);
   const { width, height } = firstPage.getSize();
   firstPage.drawText(name, {
-    x: 380,
-    y: 345,
+    x: 400,
+    y: 325,
     size: 30,
     font: Font,
     color: rgb(0, 0, 0),
